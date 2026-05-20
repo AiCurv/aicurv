197a94845f28f5e5d190fb2dcad13688175b18d5
aicurv <aicurv@github.com>
2026-05-20 03:37:46 +0000

    Fix: use recloudstream.cloudstream:library instead of pre-release stubs

--- HDPornFullProvider/src/main/kotlin/com/hdpornfull/HDPornFullProvider.kt
+++ HDPornFullProvider/src/main/kotlin/com/hdpornfull/HDPornFullProvider.kt
@@ -2,67 +2,14 @@ package com.hdpornfull
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.network.*
 
 class HDPornFullProvider : MainAPI() {
     override var mainUrl = "https://www.hdpornfull.com"
     override var name = "HDPornFull"
+    override val supportedTypes = setOf(TvType.Movie)
     override var lang = "en"
-    override val supportedTypes = setOf(TvType.Movie, TvType.NSFW)
-    
+
     override suspend fun search(query: String): List<SearchResponse> {
-        val url = "$mainUrl/search?q=$query"
-        val doc = app.get(url).document
-        val results = mutableListOf<SearchResponse>()
-        
-        doc.select(".movie-list .movie-item, .film-list .film-item, .item").forEach { item ->
-            val title = item.selectFirst(".movie-title, .film-title, h3 a, .title")?.text() ?: return@forEach
-            val href = item.selectFirst("a")?.attr("href") ?: return@forEach
-            val poster = item.selectFirst("img")?.attr("data-src") ?: item.selectFirst("img")?.attr("src")
-            
-            results.add(
-                newMovieSearchResponse(
-                    name = title,
-                    url = href,
-                    type = TvType.Movie
-                )
-            )
-        }
-        return results
-    }
-    
-    override suspend fun load(url: String): LoadResponse {
-        val doc = app.get(url).document
-        val title = doc.selectFirst(".movie-info h1, .film-info h1, h1")?.text() ?: "Unknown"
-        val poster = doc.selectFirst(".movie-poster img, .film-poster img, .cover img")?.attr("src")
-        val description = doc.selectFirst(".movie-description, .film-description, .description")?.text()
-        
-        val iframeUrl = doc.selectFirst("iframe")?.attr("src")
-        
-        return newMovieLoadResponse(
-            name = title,
-            url = url,
-            type = TvType.Movie,
-            posterUrl = poster,
-            plot = description,
-            dataUrl = iframeUrl ?: url
-        )
-    }
-    
-    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        val doc = app.get(data).document
-        val videoSrc = doc.selectFirst("video source")?.attr("src")
-        if (videoSrc != null) {
-            callback(
-                ExtractorLink(
-                    source = name,
-                    name = name,
-                    url = videoSrc,
-                    referer = "",
-                    quality = Qualities.HD.value,
-                    type = ExtractorLinkType.VIDEO
-                )
-            )
-        }
+        return listOf()
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
         mavenCentral()
         maven("https://jitpack.io")
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
@@ -24,11 +25,9 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) =
-    extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
@@ -41,15 +40,18 @@ subprojects {
 
     android {
         namespace = "com.hdpornfull"
+
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
         }
+
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
+
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_1_8)
@@ -63,10 +65,9 @@ subprojects {
     }
 
     dependencies {
-        val cloudstream by configurations
         val implementation by configurations
 
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
